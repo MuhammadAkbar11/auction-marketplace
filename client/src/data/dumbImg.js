15a31a13bce00a5bfd47685dcb18eafa90151ec2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-06-02 19:34:45 +0700

    data gambar sementara

---
+++ client/src/data/dumbImg.js
@@ -0,0 +1,10 @@
+let dumbImages = [];
+
+for (let i = 1; i < 9; i++) {
+  dumbImages.push({
+    id: "IMAGE-" + i,
+    url: `https://swiperjs.com/demos/images/nature-${i}.jpg`,
+  });
+}
+
+export default dumbImages;
