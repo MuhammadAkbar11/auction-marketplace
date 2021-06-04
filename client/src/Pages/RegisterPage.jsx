a1fced01b5c17913ba9ee691fa23c8fab816d883
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-06-04 22:47:55 +0700

    action untuk auth

--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,27 +1,67 @@
 import React from "react";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 import BreadcrumbContainer from "../Components/Layouts/BreadcrumbsContainer";
 import InputGroupRow from "../Components/UI/InputGroupRow";
 import Loader from "../Components/UI/Loader";
+import { authRegisterAction } from "../actions/auth.actions";
+
+const registerSchema = Yup.object().shape({
+  nama: Yup.string().required("Nama belum terisi"),
+  username: Yup.string()
+    .required("Username belum terisi ")
+    .min(6, "Username harus lebih dari 6 karakter"),
+  email: Yup.string()
+    .required("Alamat E-mail belum terisi")
+    .email("Alamat E-mail tidak valid"),
+  noTelp: Yup.number("Yang diisi harus angka").required(
+    "Nomor telepon belum terisi"
+  ),
+  password: Yup.string().required("Password belum tersisi"),
+  password2: Yup.string()
+    .required("Ulangi password anda")
+    .oneOf([Yup.ref("password"), null], "Password tidak sama"),
+});
 
 const RegisterPage = () => {
-  const [disabledSubmit, setDisabledSubmit] = React.useState(false);
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [loading, setLoading] = React.useState(false);
-  const submitHandler = e => {
-    e.preventDefault();
-    setLoading(true);
-    setDisabledSubmit(true);
+  let disableButton = false;
 
-    setTimeout(() => {
-      setDisabledSubmit(false);
-      setLoading(false);
-    }, 5000);
-  };
+  const [agreeTermCond, setAgreeTermCond] = React.useState(false);
+
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector(state => state.authUser);
+  const { loading } = useSelector(state => state.authRegister);
+
+  const registerFormik = useFormik({
+    validationSchema: registerSchema,
+    initialValues: {
+      nama: "",
+      username: "",
+      email: "",
+      noTelp: "",
+      password: "",
+      password2: "",
+    },
+    onSubmit: values => {
+      dispatch(authRegisterAction(values));
+      console.log(values);
+      // history.push('/payment');
+    },
+  });
+
+  if (agreeTermCond) {
+    disableButton = false;
+    if (loading || !registerFormik.isValid) {
+      disableButton = true;
+    } else {
+      disableButton = false;
+    }
+  } else {
+    disableButton = true;
+  }
 
   return (
     <>
@@ -35,62 +75,80 @@ const RegisterPage = () => {
         <Container fluid className="px-md-8">
           <Row>
             <Col xs={12}>
-              <div class="login-register-tab-list nav">
+              <div className="login-register-tab-list nav">
                 <Link to="/masuk">
                   <h4>Masuk</h4>
                 </Link>
-                <Link class="active" to="/daftar">
+                <Link className="active" to="/daftar">
                   <h4>Daftar</h4>
                 </Link>
               </div>
             </Col>
-            <Col md={7} lg={7} className="mx-auto">
+            <Col sm={11} md={10} lg={7} className="mx-auto">
               <Card body className="px-md-4 py-3">
                 <h5 className="text-black-50 font-weight-light text-center ">
                   registrasi pengguna
                 </h5>
                 <br />
-                <Form onSubmit={submitHandler}>
+                <Form onSubmit={registerFormik.handleSubmit}>
                   <InputGroupRow
                     input1={{
-                      controlId: "nama",
+                      controlid: "nama",
                       placeholder: "",
-                      value: "",
+                      value: registerFormik.values.nama,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.nama ? true : false,
+                      errormessage: registerFormik.errors.nama,
                       label: "Nama*",
                     }}
                     input2={{
-                      controlId: "username",
+                      controlid: "username",
                       placeholder: "(min 6 karakter)",
-                      value: "",
+                      value: registerFormik.values.username,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.username ? true : false,
+                      errormessage: registerFormik.errors.username,
                       label: "Username*",
                     }}
                   />
                   <InputGroupRow
                     input1={{
-                      controlId: "email",
+                      controlid: "email",
                       type: "email",
-                      value: "",
+                      value: registerFormik.values.email,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.email ? true : false,
+                      errormessage: registerFormik.errors.email,
                       label: "Email*",
                     }}
                     input2={{
-                      controlId: "notelp",
-                      value: "",
+                      controlid: "noTelp",
+                      value: registerFormik.values.noTelp,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.noTelp ? true : false,
+                      errormessage: registerFormik.errors.noTelp,
+                      placeholder: "(Hanya angka)",
                       label: "No Hp*",
                     }}
                   />
                   <InputGroupRow
                     input1={{
-                      controlId: "password",
+                      controlid: "password",
                       type: "password",
-                      value: "",
+                      value: registerFormik.values.password,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.password ? true : false,
+                      errormessage: registerFormik.errors.password,
                       placeholder: "(min 6 karakter)",
                       label: "password*",
                     }}
                     input2={{
-                      controlId: "password2",
-                      value: "",
+                      controlid: "password2",
+                      value: registerFormik.values.password2,
+                      onChange: registerFormik.handleChange,
+                      isInvalid: registerFormik.errors.password2 ? true : false,
+                      errormessage: registerFormik.errors.password2,
                       type: "password",
-                      value: "",
                       label: "Konfirmasi password*",
                     }}
                   />
@@ -98,6 +156,11 @@ const RegisterPage = () => {
                     <Form.Check
                       type="checkbox"
                       label="Telah menyetujui syarat dan ketentuan"
+                      onChange={() =>
+                        setAgreeTermCond(prevState =>
+                          !prevState ? true : false
+                        )
+                      }
                     />
                     <Link to="syarat-ketentuan" className="ml-1">
                       Syarat dan ketentuan
@@ -108,7 +171,7 @@ const RegisterPage = () => {
                       type="submit"
                       variant="primary"
                       className=" d-flex align-items-center "
-                      disabled={disabledSubmit}
+                      disabled={disableButton}
                     >
                       {loading ? (
                         <>
@@ -120,6 +183,9 @@ const RegisterPage = () => {
                       )}
                     </Button>
                   </Form.Group>
+                  <div className=" pt-2 text-spacing-0 font-weight-light text-capitalize ">
+                    Sudah menjadi member? <Link to="/masuk">Masuk</Link>
+                  </div>
                 </Form>
               </Card>
             </Col>
