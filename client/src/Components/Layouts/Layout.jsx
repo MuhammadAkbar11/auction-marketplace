9299365855934c787e5899936b6c6d42290d1ef7
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-07-03 02:59:34 +0700

    membungkus komponen Footer dan Header didalam komponen Layout

--- client/src/Components/Layouts/Layout.jsx
+++ client/src/Components/Layouts/Layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
 const Layout = ({ children }) => {
-  return <div className="main-wrapper">{children}</div>;
+  return (
+    <div className="main-wrapper">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
