500172078e6204b0382bed11307ee366c65bed32
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-07-12 21:27:51 +0700

    menambah 2 file constants

---
+++ client/src/constants/socket.constants.js
@@ -0,0 +1,1 @@
+export const SERVER_ENDPOINT = "localhost:5000";
---
+++ client/src/constants/temp.constants.js
@@ -0,0 +1,4 @@
+export const TEMP_REQUEST = "TEMP_REQUEST";
+export const TEMP_SUCCESS = "TEMP_SUCCESS";
+export const TEMP_FAIL = "TEMP_FAIL";
+export const TEMP_RESET = "TEMP_RESET";
