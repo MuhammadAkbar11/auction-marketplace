5f33afe5555511b791b292fd9de78a7da47a9ad3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-07-12 21:26:52 +0700

    menambah state loading di custom hook validasi data

--- client/src/hooks/useIsValidData.js
+++ client/src/hooks/useIsValidData.js
@@ -5,22 +5,22 @@ import { axiosConfigAuth } from "../utils/axiosConfig";
 
 const useIsValidData = () => {
   const [isValid, setIsValid] = React.useState(false);
-
+  const [loading, setLoading] = React.useState(true);
   const { userInfo } = useSelector(state => state.authUser);
 
   React.useEffect(() => {
+    setLoading(true);
     const checkValid = async () => {
       const config = axiosConfigAuth(userInfo.token);
       const isValid = await axios.get("/api/user/check-valid-data", config);
       setIsValid(isValid.data?.isValidData);
+      setLoading(false);
     };
 
     checkValid();
-
-    return () => {};
   }, [userInfo]);
 
-  return isValid;
+  return [isValid, loading];
 };
 
 export default useIsValidData;
