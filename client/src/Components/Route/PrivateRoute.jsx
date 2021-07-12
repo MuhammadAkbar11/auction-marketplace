dfa3338306ba34e4d68abe977f265fd06ccf3f46
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-07-12 21:20:11 +0700

    redirect ke halaman login ketika data userInfo kosong

--- client/src/Components/Route/PrivateRoute.jsx
+++ client/src/Components/Route/PrivateRoute.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
 import { isLogin } from "../../utils/auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const { userInfo } = useSelector(state => state.authUser);
-
+  const history = useHistory();
   React.useLayoutEffect(() => {
-    console.log(userInfo);
+    // console.log(userInfo);
+    if (!userInfo) {
+      history.push("/akun/masuk");
+    }
   }, []);
 
   return (
