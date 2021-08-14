4c5ec8cd2eb54f6ba84aac993669ad612f4259f0
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-08-14 21:05:49 +0700

    menambah loading state pada hook area indonesia

--- client/src/hooks/useIndonesianArea.js
+++ client/src/hooks/useIndonesianArea.js
@@ -4,31 +4,95 @@ import axios from "axios";
 const apiUrl = "https://dev.farizdotid.com/api/daerahindonesia";
 
 const useIndonesianArea = (idProv, idKota, idKec) => {
+  const [loading, setLoading] = useState({
+    provinsi: false,
+    kota: false,
+    kecamatan: false,
+    kelurahan: false,
+  });
+  const [errors, setErrors] = useState({
+    provinsi: null,
+    kota: null,
+    kecamatan: null,
+    kelurahan: null,
+  });
   const [provinsi, setProvinsi] = useState([]);
   const [kota, setKota] = useState([]);
   const [kecamatan, setKecamatan] = useState([]);
   const [kelurahan, setKelurahan] = useState([]);
 
   const loadProvinsi = async () => {
+    setLoading({
+      provinsi: true,
+      kota: true,
+      kecamatan: true,
+      kelurahan: true,
+    });
     const result = await axios
       .get(`${apiUrl}/provinsi`)
-      .then(res => res.data.provinsi);
+      .then(res => {
+        setLoading(ps => ({
+          provinsi: false,
+          kota: false,
+          kecamatan: false,
+          kelurahan: false,
+        }));
+        return res.data.provinsi;
+      })
+      .catch(err => {
+        setErrors(ps => ({ ...ps, provinsi: err }));
+        setLoading(ps => ({
+          provinsi: false,
+          kota: false,
+          kecamatan: false,
+          kelurahan: false,
+        }));
+        return [];
+      });
     setProvinsi(result);
   };
 
   const loadKota = async (query, setVal = false, callback) => {
+    setLoading(ps => ({ ...ps, kota: true, kecamatan: true, kelurahan: true }));
     const result = await axios
       .get(`${apiUrl}/kota?id_provinsi=${query}`)
-      .then(res => res.data.kota_kabupaten);
+      .then(res => {
+        setLoading(ps => ({
+          ...ps,
+          kota: false,
+          kecamatan: false,
+          kelurahan: false,
+        }));
+        return res.data.kota_kabupaten;
+      })
+      .catch(err => {
+        setErrors(ps => ({ ...ps, kota: err }));
+        setLoading(ps => ({
+          ...ps,
+          kota: false,
+          kecamatan: false,
+          kelurahan: false,
+        }));
+        return [];
+      });
     setKota(result);
     if (setVal) {
       callback && callback(result);
     }
   };
   const loadKecamatan = async (query, setVal = false, callback) => {
+    setLoading(ps => ({ ...ps, kecamatan: true, kelurahan: true }));
     const result = await axios
       .get(`${apiUrl}/kecamatan?id_kota=${query}`)
-      .then(res => res.data.kecamatan);
+      .then(res => {
+        setLoading(ps => ({ ...ps, kecamatan: false, kelurahan: false }));
+        return res.data.kecamatan;
+      })
+      .catch(err => {
+        setErrors(ps => ({ ...ps, kecamatan: err }));
+        setLoading(ps => ({ ...ps, kecamatan: false, kelurahan: false }));
+        return [];
+      });
     setKecamatan(result);
     if (setVal) {
       callback && callback(result);
@@ -36,9 +100,18 @@ const useIndonesianArea = (idProv, idKota, idKec) => {
   };
 
   const loadKelurahan = async (query, setVal = false, callback) => {
+    setLoading(ps => ({ ...ps, kelurahan: true }));
     const result = await axios
       .get(`${apiUrl}/kelurahan?id_kecamatan=${query}`)
-      .then(res => res.data.kelurahan);
+      .then(res => {
+        setLoading(ps => ({ ...ps, kelurahan: false }));
+        return res.data.kelurahan;
+      })
+      .catch(err => {
+        setErrors(ps => ({ ...ps, kelurahan: err }));
+        setLoading(ps => ({ ...ps, kelurahan: false }));
+        return [];
+      });
 
     setKelurahan(result);
     if (setVal) {
@@ -48,13 +121,23 @@ const useIndonesianArea = (idProv, idKota, idKec) => {
 
   useEffect(() => {
     loadProvinsi();
+  }, []);
+
+  useEffect(() => {
     loadKota(idProv);
+  }, [idProv]);
+
+  useEffect(() => {
     loadKecamatan(idKota);
-    console.log(idProv, idKota, idKec);
+  }, [idKota]);
+
+  useEffect(() => {
     loadKelurahan(idKec);
-  }, [idProv, idKota, idKec]);
+  }, [idKec]);
 
   const indonesiaArea = {
+    loading,
+    errors,
     provinsi,
     kota,
     kecamatan,
