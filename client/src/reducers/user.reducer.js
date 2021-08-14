b7606b164afbcdf5ec4934a204b05cadede18fda
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-08-14 20:42:12 +0700

    reducer untk brang yang terjual

--- client/src/reducers/user.reducer.js
+++ client/src/reducers/user.reducer.js
@@ -30,6 +30,9 @@ import {
   USER_COMPLETE_LIST_AUCTION_REQ,
   USER_COMPLETE_LIST_AUCTION_SUCCESS,
   USER_COMPLETE_LIST_AUCTION_FAIL,
+  USER_SOLDITEM_REQ,
+  USER_SOLDITEM_SUCCESS,
+  USER_SOLDITEM_FAIL,
   USER_AUCTION_MESSAGE,
   USER_AUCTION_RESET_MESSAGE,
   USER_DELETE_AUCTION_REQ,
@@ -42,6 +45,12 @@ import {
   USER_CONFIRM_BID_SUCCESS,
   USER_CONFIRM_BID_FAIL,
   ADD_DELIVERY_AUCTION,
+  USER_SOLD_ITEM_INFO_REQ,
+  USER_SOLD_ITEM_INFO_SUCCESS,
+  USER_SOLD_ITEM_INFO_FAIL,
+  USER_UPDATE_BILL_REQ,
+  USER_UPDATE_BILL_SUCCESS,
+  USER_UPDATE_BILL_FAIL,
 } from "../constants/user.contanst";
 
 export const userDetailsReducer = (
@@ -53,7 +62,7 @@ export const userDetailsReducer = (
 ) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
-      return { ...state, ...action.payload };
+      return { loading: true, details: {} };
     case USER_DETAILS_SUCCESS:
       return {
         ...state,
@@ -224,6 +233,21 @@ export const userAuctionReducer = (state = userActionInitState, action) => {
         ...state,
         completeList: action.payload,
       };
+    case USER_SOLDITEM_REQ:
+      return {
+        ...state,
+        soldItems: action.payload,
+      };
+    case USER_SOLDITEM_SUCCESS:
+      return {
+        ...state,
+        soldItems: action.payload,
+      };
+    case USER_SOLDITEM_FAIL:
+      return {
+        ...state,
+        soldItems: action.payload,
+      };
     case USER_AUCTION_MESSAGE:
       return {
         ...state,
@@ -375,3 +399,52 @@ export const userConfirmBidReducer = (
       return state;
   }
 };
+
+export const userSoldItemDetailsReducer = (
+  state = {
+    loading: false,
+    details: null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case USER_SOLD_ITEM_INFO_REQ:
+      return {
+        loading: true,
+      };
+    case USER_SOLD_ITEM_INFO_SUCCESS:
+      return {
+        ...action.payload,
+      };
+    case USER_SOLD_ITEM_INFO_FAIL:
+      return {
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// export const userUpdateBillReducer = (
+//   state = {
+//     loading: false,
+//   },
+//   action
+// ) => {
+//   switch (action.type) {
+//     case USER_UPDATE_BILL_REQ:
+//       return {
+//         loading: true,
+//       };
+//     case USER_UPDATE_BILL_SUCCESS:
+//       return {
+//         loading: false,
+//       };
+//     case USER_UPDATE_BILL_FAIL:
+//       return {
+//         ...action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
