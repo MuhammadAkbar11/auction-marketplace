1ca9fe0ee64d6628d1e694c9dd5344f399a79be1
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-10-24 12:34:33 +0700

    refactor UserWrapperIsValidata compnent

--- client/src/Components/UserWrapperIsValidData.jsx
+++ client/src/Components/UserWrapperIsValidData.jsx
@@ -1,27 +1,73 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDetailsAction } from "../actions/user.actions";
 import useIsValidData from "../hooks/useIsValidData";
 import ModalInvalidData from "./ModalInvalidData";
 import Loader from "./UI/Loader";
 
 const UserWrapperIsValidData = ({ children }) => {
-  const [modal, setModal] = React.useState(false);
+  const [modal, setModal] = React.useState({
+    show: false,
+  });
+  const [invalid, setInvalid] = React.useState(false);
 
+  /*
+
+title: "Akses ditolak!",
+  message: "Silahkan lengkapi data diri anda untuk melajutkan!",
+  linkTo: "/akun/info",
+  linkText: "Lengkapi data diri",
+
+*/
+  const dispatch = useDispatch();
   const [isValidData, loadingValidData] = useIsValidData();
+  const userDetailsState = useSelector(state => state.userDetails);
 
   React.useEffect(() => {
+    dispatch(getUserDetailsAction());
+  }, []);
+
+  React.useEffect(() => {
+    const userBankAccounts = userDetailsState?.details.akun_bank;
     if (!isValidData) {
-      setModal(true);
+      setInvalid(true);
+      setModal({
+        show: true,
+      });
+    } else {
+      if (userBankAccounts?.length === 0) {
+        setInvalid(true);
+        setModal({
+          show: true,
+          title: "Daftar nomor rekening masih kosong!",
+          message:
+            "Silahkan daftarkan nomor rekening anda supaya dapat melanjutkan proses ini!",
+          linkTo: "/akun/info#bank-accounts",
+          linkText: "Daftar nomor rekening",
+        });
+      } else {
+        setInvalid(false);
+      }
     }
 
     return () => {
       setModal(false);
     };
-  }, [isValidData]);
-
-  return isValidData ? (
+  }, [isValidData, userDetailsState]);
+  return !invalid ? (
     <React.Fragment>{children}</React.Fragment>
   ) : (
-    <>{!loadingValidData ? <ModalInvalidData /> : null}</>
+    <>
+      {!loadingValidData ? (
+        <ModalInvalidData
+          show={modal.show}
+          title={modal?.title || null}
+          message={modal?.message || null}
+          linkTo={modal?.linkTo || null}
+          linkText={modal?.linkText || null}
+        />
+      ) : null}
+    </>
   );
 };
 
