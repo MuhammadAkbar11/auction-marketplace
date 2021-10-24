ba35fdf78737f0f2eb00e0e2e0eb6a22c7650cc6
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-10-24 22:38:45 +0700

    membuat objek utk data bulan

---
+++ client/src/data/month.js
@@ -0,0 +1,16 @@
+const months = {
+  1: "Januari",
+  2: "Februari",
+  3: "Maret",
+  4: "April",
+  5: "Mei",
+  6: "Juni",
+  7: "Juli",
+  8: "Agustus",
+  9: "September",
+  10: "Oktober",
+  11: "November",
+  12: "Desember",
+};
+
+export default months;
