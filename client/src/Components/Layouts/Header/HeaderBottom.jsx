467174f5a046eaae6eb5310cb0544bb9b9d0b049
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-16 16:29:44 +0700

    show more and show less categories on dropdown topbar

--- client/src/Components/Layouts/Header/HeaderBottom.jsx
+++ client/src/Components/Layouts/Header/HeaderBottom.jsx
@@ -12,8 +12,20 @@ import { ListIcon } from "../../UI/Icons/Index";
 import { useSelector } from "react-redux";
 
 const HeaderBottom = () => {
+  const [moreCategories, setMoreCategories] = React.useState(false);
+
   const { categories } = useSelector(state => state.categories);
-  const spliceCategories = [...categories];
+
+  let leftCategories =
+    categories.length >= 5 && !moreCategories
+      ? [...categories].splice(0, 5)
+      : [...categories];
+  const categoriesSelected = [...categories];
+
+  // if(!moreCategories) {
+  //   leftCategories.splice(0, 10)
+  // }
+
   return (
     <div className="headerBottom">
       <Container fluid className="px-md-8">
@@ -29,9 +41,21 @@ const HeaderBottom = () => {
                 <ListIcon size="27" />
                 <span>All Kategori</span>
               </Dropdown.Toggle>{" "}
-              <Dropdown.Menu className="border-0 pt-0 w-100 mainCategoriMenu shadow-sm ">
+              <Dropdown.Menu
+                style={{
+                  height:
+                    categories.length >= 5 && !moreCategories
+                      ? "max-content"
+                      : 425,
+                  overflowY:
+                    categories.length >= 5 && !moreCategories
+                      ? "hidden"
+                      : "scroll",
+                }}
+                className="border-0 pt-0 w-100 mainCategoriMenu shadow-sm "
+              >
                 <ListGroup className="border-0 text-dark font-weight-bolder mainCategoriList ">
-                  {categories.map(item => {
+                  {leftCategories.map(item => {
                     return (
                       <ListGroup.Item
                         key={item.id_kategori}
@@ -43,6 +67,18 @@ const HeaderBottom = () => {
                       </ListGroup.Item>
                     );
                   })}
+                </ListGroup>
+                <ListGroup className=" border-0 text-center ">
+                  <a
+                    href="#/"
+                    onClick={ev => {
+                      ev.preventDefault();
+                      setMoreCategories(!moreCategories);
+                    }}
+                    className=" text-gray-700 underline"
+                  >
+                    <small>{moreCategories ? "show less" : "show more"} </small>
+                  </a>
                 </ListGroup>
               </Dropdown.Menu>
             </Dropdown>
@@ -60,7 +96,7 @@ const HeaderBottom = () => {
               </div>
               <div className="categoriSelect">
                 <Form.Control as="select">
-                  {spliceCategories.splice(0, 5).map(item => {
+                  {categoriesSelected.splice(0, 5).map(item => {
                     return (
                       <option key={item.id_kategori}>{item.kategori}</option>
                     );
