f7bcef7a7b55ec8627f93e1aaa51f15605d25503
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-18 23:52:47 +0700

    admin index page

---
+++ client/src/Pages/Admin/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+const AdminMainPage = () => {
+  return <Redirect to="/administrator/dashboard" />;
+};
+
+export default AdminMainPage;
